2f05b6da9579aa68d881a902536dff4de721f597
constanze <constanze.maass@haw-hamburg.de>
2019-09-14 01:46:35 +0200

    first

---
+++ src/main/java/com/project/haw/HawApplication.java
@@ -0,0 +1,13 @@
+package com.project.haw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawApplication.class, args);
+	}
+
+}
