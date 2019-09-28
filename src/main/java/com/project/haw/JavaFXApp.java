c7ddddd8de162470deae28df4304e0b1293cc051
constanze <constanze.maass@haw-hamburg.de>
2019-09-28 09:37:09 +0200

    [Config] JavaFX

---
+++ src/main/java/com/project/haw/JavaFXApp.java
@@ -0,0 +1,26 @@
+package com.project.haw;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class JavaFXApp extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+
+        Label label = new Label("Hello World, JavaFX !");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        JavaFXApp.launch(args);
+    }
+}
--- src/main/java/com/project/haw/WebSSRApp.java
+++ src/main/java/com/project/haw/WebSSRApp.java
@@ -3,11 +3,12 @@ package com.project.haw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Server Side Rendering App
 @SpringBootApplication
-public class WebApp {
+public class WebSSRApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebApp.class, args);
+		SpringApplication.run(WebSSRApp.class, args);
 	}
 
 }
